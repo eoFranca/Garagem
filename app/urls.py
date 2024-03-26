cd731c5f9696ac103ebe73bf99303ab95b9f40e3
eoFranca <erfranca.com@gmail.com>
2024-03-26 09:04:57 -0300

    Criação das API Acessorios,Categorias e Cores

--- app/urls.py
+++ app/urls.py
@@ -10,9 +10,13 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
+from core.views import AcessorioViewSet ,CategoriaViewSet ,CorViewSet
 
 router = DefaultRouter()
 
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias",CategoriaViewSet)
+router.register(r"cores",CorViewSet)
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,1 +1,4 @@
 from .user import UserViewSet
+from .acessorio import AcessorioViewSet
+from .categoria import CategoriaViewSet
+from .cor import CorViewSet
